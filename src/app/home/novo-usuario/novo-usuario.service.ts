a801a136e789b4637c4133066556f0c9785ee928
Guilherme Santana <guilhermesantana84@hotmail.com>
2022-03-01 23:11:43 -0300

    Commit de backup

---
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { stringify } from 'querystring';
+import { Observable } from 'rxjs';
+import { NovoUsuario } from './novo-usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NovoUsuarioService {
+
+  urlPost = 'http://localhost:3000/user/singup';
+  
+  constructor(private httpClient: HttpClient) { }
+
+  cadastraNovoUsuario(novoUsuario: NovoUsuario) :Observable<any> {
+    return this.httpClient.post(this.urlPost, novoUsuario);
+  }
+
+  verificaUsuarioExistente(nomeUsuario : string) :Observable<any> {
+      return this.httpClient.get(`http://localhost:3000/user/exists/${nomeUsuario}`);
+  }
+}
+
